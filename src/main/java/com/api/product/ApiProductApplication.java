a52c373a9dc361bcbae99f4c69935c50cfb50d25
RenatoSoutoMaior <renatosoutomaior18@gmail.com>
2017-08-21 19:49:55 -0300

    Corrigindo configuracoes iniciais

--- src/main/java/com/api/product/ApiProductApplication.java
+++ src/main/java/com/api/product/ApiProductApplication.java
@@ -2,14 +2,8 @@ package com.api.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableDiscoveryClient
-@EnableJpaRepositories
 public class ApiProductApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/api/product/model/Product.java
+++ src/main/java/com/api/product/model/Product.java
@@ -1,15 +1,13 @@
 package com.api.product.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-import java.io.Serializable;
 import java.math.BigDecimal;
 
-@MappedSuperclass
-public class Product implements Serializable {
-    private static final long serialVersionUID = 1L;
+@Entity
+public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
