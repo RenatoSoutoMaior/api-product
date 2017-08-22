47a002d6a16254d6f271306c7d0773e3bbad11cb
RenatoSoutoMaior <renatosoutomaior18@gmail.com>
2017-08-21 23:11:05 -0300

    Refatorando Produto

--- src/main/java/com/api/product/model/Product.java
+++ src/main/java/com/api/product/model/Product.java
@@ -17,13 +17,29 @@ public class Product {
     private String description;
     private BigDecimal price;
     private BigDecimal quantity;
+    private Category category;
+    private Status status;
     private String user;
     private String image;
     private BigDecimal weight;
     private BigDecimal unit;
-    private Category category;
-    private Status status;
     private String shelfLife;
+
+    public Product(String name, String description, BigDecimal price, BigDecimal quantity,
+                   Category category, Status status, String user, String image,
+                   BigDecimal weight, BigDecimal unit, String shelfLife) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.status = status;
+        this.user = user;
+        this.image = image;
+        this.weight = weight;
+        this.unit = unit;
+        this.shelfLife = shelfLife;
+    }
 
     public BigDecimal getId() {
         return id;
@@ -63,5 +79,61 @@ public class Product {
 
     public void setQuantity(BigDecimal quantity) {
         this.quantity = quantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getWeight() {
+        return weight;
+    }
+
+    public void setWeight(BigDecimal weight) {
+        this.weight = weight;
+    }
+
+    public BigDecimal getUnit() {
+        return unit;
+    }
+
+    public void setUnit(BigDecimal unit) {
+        this.unit = unit;
+    }
+
+    public String getShelfLife() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(String shelfLife) {
+        this.shelfLife = shelfLife;
     }
 }
