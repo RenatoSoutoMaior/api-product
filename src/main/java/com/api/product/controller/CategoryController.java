f91793f1f4ac156f8e4ce1825ab93f34049736ec
renato.souto <renato.souto@matera.com>
2017-10-17 15:23:20 -0200

    Criando controller para Categoria

---
+++ src/main/java/com/api/product/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.api.product.controller;
+
+import com.api.product.model.Category;
+import com.api.product.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @GetMapping(value = "/product/category")
+    public List<Category> listAll() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping(value = "/product/category/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Category findById(@PathVariable("id") BigDecimal id) {
+        return categoryRepository.findOne(id);
+    }
+
+    @GetMapping(value = "/api/product/category/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Category> findByName(@PathVariable("name") String name) {
+        return categoryRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    @PostMapping(value = "/product/category")
+    public void create(@RequestBody Category product) {
+        categoryRepository.save(product);
+    }
+
+    @PutMapping(value = "/product/category/{id}")
+    public void update(@RequestBody Category product) {
+        categoryRepository.saveAndFlush(product);
+    }
+
+    @DeleteMapping(value = "/product/category/{id}")
+    public void delete(@PathVariable("id") BigDecimal id) {
+        categoryRepository.delete(id);
+    }
+}
